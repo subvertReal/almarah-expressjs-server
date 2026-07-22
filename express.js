7b56f33d0c40791d56cf5b5ec36ff6fd78888103
subvertReal <sherjeelk2004@gmail.com>
2026-07-22 10:55:53 -0400

    Created route for /public folder and route for /scan which gets information of each file in /public and returns it as a json file

---
+++ express.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express()
+const port = 3000
+
+app.use('/static', express.static('public'));
+
+app.get('/scan',(req, res) => {
+  const imagesDir = path.join(__dirname, 'public');
+    fs.readdir(imagesDir, (err, files) => {
+            if (err) {
+                return res.status(500).json({ error: 'Unable to read images directory' });
+            }
+            // Filter for image files (webp)
+            const imageFiles = files.filter(file =>
+                /\.(webp)$/i.test(file)
+            );
+            // Read and encode each image as base64
+            const imageScan = imageFiles.map(file => {
+                return {
+                    name: file,
+                };
+            });
+            res.json(imageScan);
+        });
+    
+
+})
+
+app.listen(port, () => {
+  console.log(`Express server is listening on port http://localhost:${port}`)
+})
