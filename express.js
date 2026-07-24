d708142abc8a5e9579b3c4c174d2b4f9f8a14913
subvertReal <sherjeelk2004@gmail.com>
2026-07-24 17:51:39 -0400

    added route for scanning showcase-one images with almost the same logic of slideshow scan

--- express.js
+++ express.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const app = express()
 const port = 3000
 
+// makes public folder images accessable
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
@@ -12,8 +13,8 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
+//! urls that read images in specific folders in /public and sends back the names of the files
+//? homepage components
 app.get('/scan/slideshow',(req, res) => {
   const imagesDir = path.join(__dirname, 'public/slideshow');
     fs.readdir(imagesDir, (err, files) => {
@@ -33,8 +34,30 @@ app.get('/scan/slideshow',(req, res) => {
             res.json(imageScan);
         });
     
-
 });
+
+app.get('/scan/showcase-one',(req, res) => {
+  const imagesDir = path.join(__dirname, 'public/ShowCaseOne');
+    fs.readdir(imagesDir, (err, files) => {
+            if (err) {
+                return res.status(500).json({ error: 'Unable to read images directory' });
+            }
+            // Filter for image files (webp)
+            const imageFiles = files.filter(file =>
+                /\.(webp)$/i.test(file)
+            );
+            // Read and encode each image as base64
+            const imageScan = imageFiles.map(file => {
+                return {
+                    name: file,
+                };
+            });
+            res.json(imageScan);
+        });
+    
+});
+
+
 
 app.listen(port, () => {
   console.log(`Express server is listening on port http://localhost:${port}`)
