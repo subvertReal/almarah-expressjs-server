9784cb6132b876f1de5011852062addff555875d
subvertReal <sherjeelk2004@gmail.com>
2026-08-04 18:25:26 -0400

    Added better-sqlite3 and started converting the express js server to use it instead of scanning the folders (so that stuff like names and desc can be attributed to an image)

--- express.js
+++ express.js
@@ -1,6 +1,37 @@
+const Database = require('better-sqlite3');
+
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+
+let dbPath = path.join(__dirname, 'clothing.db');
+if (!fs.existsSync(dbPath)) {
+    let db = new Database(dbPath);
+    db.pragma('journal_mode = WAL');
+    db.exec(`CREATE TABLE clothing (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category TEXT NOT NULL,
+        clothingType TEXT NOT NULL,
+        name TEXT NOT NULL,
+        desc TEXT NOT NULL,
+        price REAL check(price >= 0),
+        pic1 TEXT NOT NULL,
+        pic2 TEXT,
+        pic3 TEXT
+
+    )`)
+    db.close();
+}
+else{
+    const db = new Database(dbPath);
+    db.pragma('journal_mode = WAL');
+    db.close();
+}
+
+
+
+
 
 const app = express()
 const port = 3000
@@ -12,6 +43,21 @@ app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
+
+// only for development purposes
+app.get('/all', (req,res)=>{
+    let dbPath = path.join(__dirname, 'clothing.db');
+    let db = new Database(dbPath);
+    
+    const row = db.prepare('SELECT * FROM clothing').all();
+    // const row = db.prepare('SELECT * FROM clothing WHERE id = ?').get(1);
+    console.log(row);
+
+    res.json(row);
+
+    db.close();
+})
+
 
 //! urls that read images in specific folders in /public and sends back the names of the files
 //? homepage components
@@ -58,26 +104,38 @@ app.get('/scan/showcase-one',(req, res) => {
 });
 
 //?mens Clothing components
+// app.get('/scan/shalwar-kameez',(req, res) => {
+//   const imagesDir = path.join(__dirname, 'public/menClothing/shalwarKameez');
+//     fs.readdir(imagesDir, (err, files) => {
+//             if (err) {
+//                 return res.status(500).json({ error: 'Unable to read images directory' });
+//             }
+//             // Filter for image files (webp)
+//             const imageFiles = files.filter(file =>
+//                 /\.(webp)$/i.test(file)
+//             );
+//             // Read and encode each image as base64
+//             const imageScan = imageFiles.map(file => {
+//                 return {
+//                     name: file,
+//                 };
+//             });
+//             res.json(imageScan);
+//         });
+    
+// });
 app.get('/scan/shalwar-kameez',(req, res) => {
-  const imagesDir = path.join(__dirname, 'public/menClothing/shalwarKameez');
-    fs.readdir(imagesDir, (err, files) => {
-            if (err) {
-                return res.status(500).json({ error: 'Unable to read images directory' });
-            }
-            // Filter for image files (webp)
-            const imageFiles = files.filter(file =>
-                /\.(webp)$/i.test(file)
-            );
-            // Read and encode each image as base64
-            const imageScan = imageFiles.map(file => {
-                return {
-                    name: file,
-                };
-            });
-            res.json(imageScan);
-        });
+     let dbPath = path.join(__dirname, 'clothing.db');
+    let db = new Database(dbPath);
+    
+    // const row = db.prepare('SELECT * FROM clothing').all();
+    const row = db.prepare('SELECT * FROM clothing WHERE clothingType = ?').all('shalwarKameez');
+    console.log(row);
+    
+    res.json(row);
     
 });
+
 app.get('/scan/waistcoat',(req, res) => {
   const imagesDir = path.join(__dirname, 'public/menClothing/waistcoat');
     fs.readdir(imagesDir, (err, files) => {
